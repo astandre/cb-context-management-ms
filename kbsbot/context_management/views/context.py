32175337b0cc1d64b6c992b78e054d6e84f2dd3d
Andre <andreherrera97@hotmail.com>
2020-02-07 14:56:08 -0500

    removing channel

--- kbsbot/context_management/views/context.py
+++ kbsbot/context_management/views/context.py
@@ -43,7 +43,6 @@ def find_entity_context():
                 found_entities = []
         else:
             found_entities = []
-        result["channel"] = data["channel"]
         result["user"] = data["user"]
         result["entities"] = found_entities
         logger.info("<<<<<< OUTPUT  %s", result)
